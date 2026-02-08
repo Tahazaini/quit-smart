47789bf0d191f9c0a0b981db1598469e9e6c57df
Tahazaini <46445802+Tahazaini@users.noreply.github.com>
2026-02-08 19:03:55 +0300

    Create layout.tsx

---
+++ app/app/app/layout.tsx
@@ -0,0 +1,16 @@
+export const metadata = {
+  title: "QuitSmart",
+  description: "QuitSmart MVP",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
